f0d4115f888f9b4b352bc984a6f64116f903ceb6
sarun tapee <tpzrun@gmail.com>
2024-10-30 20:48:40 -0500

    init project

---
+++ src/test/java/edu/miu/waa/lab/LabApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.waa.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
